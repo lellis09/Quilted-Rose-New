684666481b242cb8214bacd705925c23b42182aa
unknown <lishia.ellis09@gmail.com>
2025-03-19 18:38:07 -0400

    removed border from logo

--- components/sections/logo-cont.jsx
+++ components/sections/logo-cont.jsx
@@ -5,7 +5,7 @@ import Nav from "../layout/nav";
 const LogoContainer = () => {
     return(
         <section className="logo-container w-full h-64 sm:h-auto  bg-creamy flex flex-col justify-center items-center">
-            <div className="border-2 border-blush w-96 sm:h-64 flex flex-col justify-center relative">
+            <div className=" w-96 sm:h-64 flex flex-col justify-center relative">
                 <Image src={"/qr-logo.svg"} width={400} height={400} className="bottom-6 lg:bottom-0"/>
             </div>
             <div>
