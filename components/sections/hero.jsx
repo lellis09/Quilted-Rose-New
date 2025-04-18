d875b1b2e5eb89ca02b1706825411bfbfaad0ea7
unknown <lishia.ellis09@gmail.com>
2025-04-18 09:11:10 -0400

    using webp file for hero image-test load speed

--- components/sections/hero.jsx
+++ components/sections/hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({ title, imageSrc}) => {
     return(
         <section className="hero w-full h-72 md:h-[25rem] bg-darkGreen flex justify-center items-center relative overflow-hidden ">
                <Image 
-                src={'/florals-1.jpg'}
+                src={'/florals-new.webp'}
                 // width={500}
                 // height={500}
                 layout="fill"
