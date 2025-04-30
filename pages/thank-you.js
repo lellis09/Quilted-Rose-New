4448fc9aa36fb198474b25cae41104e06247dd9e
unknown <lishia.ellis09@gmail.com>
2025-04-30 13:16:33 -0400

    still working on the issue with netlify form

---
+++ pages/thank-you.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { pages } from "/content/content.jsx";  // Import content data from content.jsx
+import Image from "next/image";
+import Button from "../components/Button";
+
+const ThankYou = () => {
+    return (
+        <main>
+            Thank you!
+        </main>
+    )
+}
+export default ThankYou
