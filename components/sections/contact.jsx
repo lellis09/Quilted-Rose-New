c09e4ff9cf44e52cc36518683e1a6b82339e95de
unknown <lishia.ellis09@gmail.com>
2025-04-30 20:23:09 -0400

    still trying to fix netlify form-went back to my original codeo

--- components/sections/contact.jsx
+++ components/sections/contact.jsx
@@ -5,31 +5,6 @@ import { pages } from "/content/content.jsx"
 import Button from "../Button.js";
 
 
-
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-        setStatus('pending');
-        setError(null);
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-        const res = await fetch('/__forms.html', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams(formData).toString()
-        });
-        if (res.status === 200) {
-            setStatus('ok');
-        } else {
-            setStatus('error');
-            setError(`${res.status} ${res.statusText}`);
-        }
-    } catch (e) {
-        setStatus('error');
-        setError(`${e}`);
-    }
-};
-
 const Contact = ({title}) => {
     return(
         <main className="flex flex-col md:flex-row bg-olive w-full md:h-[500px] h-full">
@@ -47,24 +22,7 @@ const Contact = ({title}) => {
                 />
             </div>
 
-    return (
-        <div className="w-full md:max-w-md">
-            <Card title="Leave Feedback">
-                <form name="contact" onSubmit={handleFormSubmit} className="flex flex-col gap-3 align-center">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <input name="name" type="text" placeholder="Name" required className="input" />
-                    <input name="email" type="email" placeholder="Email (optional)" className="input" />
-                    <textarea name="message" type="text" placeholder="Message" required className="input" />
-                    <button className="btn" type="submit" disabled={status === 'pending'}>
-                        Submit
-                    </button>
-                    {status === 'ok' && <Alert type="success">Submitted!</Alert>}
-                    {status === 'error' && <Alert type="error">{error}</Alert>}
-                </form>
-            </Card>
-        </div>
-    );
-            {/* <section className="flex flex-col bg-olive items-center px-10 gap-y-6 w-full h-[500px] md:w-[60%] justify-center">
+               <section className="flex flex-col bg-olive items-center px-10 gap-y-6 w-full h-[500px] md:w-[60%] justify-center">
                 <h2 className="text-creamy text-5xl pt-6">{title}</h2>
 
                 <form
@@ -106,7 +64,7 @@ const Contact = ({title}) => {
                         Get In Touch
                     </Button>
                 </form>
-        </section> */}
+        </section>
             
         </main>
     )
